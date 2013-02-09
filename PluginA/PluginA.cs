4480a10d905918e155f949eeb6fd0c9f046938a2
Jared Parsons <jaredpar@microsoft.com>
2013-02-09 13:04:38 -0800

    Simple VSIX example
    Tested the SimpleUtil to made sure it works (no stupid coding errors in
    the tag logic)

---
+++ PluginA/PluginA.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using SimpleUtil;
+
+namespace PluginA
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("text")]
+    [TagType(typeof(TextMarkerTag))]
+    internal sealed class PluginATaggerProvider : ITaggerProvider
+    {
+        private readonly ITagFinder _tagFinder;
+
+        [ImportingConstructor]
+        internal PluginATaggerProvider([Import(SimpleUtilConstants.Contract)]ITagFinder tagFinder)
+        {
+            _tagFinder = tagFinder;
+        }
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return (ITagger<T>)(object)new PluginATagger(_tagFinder);
+        }
+    }
+
+    internal sealed class PluginATagger : ITagger<TextMarkerTag>
+    {
+        private readonly ITagFinder _tagFinder;
+
+        internal PluginATagger(ITagFinder tagFinder)
+        {
+            _tagFinder = tagFinder;
+        }
+
+        public IEnumerable<ITagSpan<TextMarkerTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            var tag = new TextMarkerTag("PluginA");
+            foreach (var span in spans)
+            {
+                foreach (var foundSpan in _tagFinder.FindTags(span))
+                {
+                    var tagSpan = new TagSpan<TextMarkerTag>(foundSpan, tag);
+                    yield return tagSpan;
+                }
+            }
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "PluginA")]
+    [Name("PluginA")]
+    [UserVisible(true)]
+    [Order(Before = Priority.Default)] 
+    internal sealed class PluginAFormat : ClassificationFormatDefinition
+    {
+        internal PluginAFormat()
+        {
+            this.DisplayName = "PluginA"; //human readable version of the name
+            this.BackgroundColor = Colors.BlueViolet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    internal static class PluginAClassificationDefinition
+    {
+        /// <summary>
+        /// Defines the "PluginA" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("PluginA")]
+        internal static ClassificationTypeDefinition PluginAType = null;
+    }
+}
---
+++ SimpleUtil/Components.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
+
+namespace SimpleUtil
+{
+    public interface ITagFinder
+    {
+        ReadOnlyCollection<SnapshotSpan> FindTags(SnapshotSpan span);
+    }
+
+    public static class SimpleUtilConstants
+    {
+        public const string Contract = "SimpleUtil_1.0";
+    }
+
+    [Export(SimpleUtilConstants.Contract, typeof(ITagFinder))]
+    internal sealed class TagFinder : ITagFinder
+    {
+        private const string Target = "component a";
+        private IEnumerable<SnapshotSpan> FindTags(SnapshotSpan span)
+        {
+            var snapshot = span.Snapshot;
+            var i = 0;
+            while (i < span.Length)
+            {
+                var position = i + span.Start.Position;
+                if (IsMatch(snapshot, position))
+                {
+                    yield return new SnapshotSpan(snapshot, position, Target.Length);
+                    i += span.Length;
+                }
+                else
+                {
+                    i += 1;
+                }
+            }
+        }
+
+        private bool IsMatch(ITextSnapshot snapshot, int position)
+        {
+            if (position + Target.Length >= snapshot.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Target.Length; i++)
+            {
+                var source = snapshot[position + i];
+                if (Char.ToLower(source) != Target[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        ReadOnlyCollection<SnapshotSpan> ITagFinder.FindTags(SnapshotSpan span)
+        {
+            return new ReadOnlyCollection<SnapshotSpan>(FindTags(span).ToList());
+        }
+    }
+}
